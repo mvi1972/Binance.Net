7a44896c711baf43a42bcc2e76e6e725fdf56593
vv <4590955@list.ru>
2020-07-09 11:37:04 +0300

    Тестирование навигационной панели

---
+++ Examples/Test/ApplicationToolbar.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test
+{
+    /// <summary>
+    /// Логика взаимодействия для ApplicationToolbar.xaml
+    /// </summary>
+    public partial class ApplicationToolbar : UserControl
+    {
+        public ApplicationToolbar()
+        {
+            InitializeComponent();
+        }
+
+        private void NavigationCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            NavigationService service = NavigationService.GetNavigationService(this);
+            service.Navigate(new Uri(e.Parameter.ToString(), UriKind.Relative));
+        }
+    }
+}
